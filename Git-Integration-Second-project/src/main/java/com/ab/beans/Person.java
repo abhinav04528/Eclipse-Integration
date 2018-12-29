ef73fc4b4842b329248bafb207c4f62057c76b7a
Abhinav <Abhinav@DESKTOP-CA5G3BU>
2018-12-29 22:54:54 +0530

    Second Project Bean Folder Commit..

---
+++ Git-Integration-Second-project/src/main/java/com/ab/beans/Person.java
@@ -0,0 +1,73 @@
+/**
+ * 
+ */
+package com.ab.beans;
+
+/**
+ * @author Abhinav
+ *  @Date Dec 29, 2018
+ * @Time 9:58:46 PM
+ */
+public class Person {
+
+	private int empId;
+	private String empName;
+	private String empAddress;
+	private float salary;
+
+	/**
+	 * @return the empId
+	 */
+	public int getEmpId() {
+		return empId;
+	}
+
+	/**
+	 * @param empId the empId to set
+	 */
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	/**
+	 * @return the empName
+	 */
+	public String getEmpName() {
+		return empName;
+	}
+
+	/**
+	 * @param empName the empName to set
+	 */
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	/**
+	 * @return the empAddress
+	 */
+	public String getEmpAddress() {
+		return empAddress;
+	}
+
+	/**
+	 * @param empAddress the empAddress to set
+	 */
+	public void setEmpAddress(String empAddress) {
+		this.empAddress = empAddress;
+	}
+
+	/**
+	 * @return the salary
+	 */
+	public float getSalary() {
+		return salary;
+	}
+
+	/**
+	 * @param salary the salary to set
+	 */
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+}
